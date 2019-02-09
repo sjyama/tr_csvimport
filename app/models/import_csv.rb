8f994ff5bc05edf171a5c1a961ece97605bb4691
sjyama_mbp <grinpeace101@gmail.com>
2019-02-09 10:57:19 +0900

    add CSVimport

---
+++ app/models/import_csv.rb
@@ -0,0 +1,11 @@
+class ImportCsv < ApplicationRecord
+	def self.import(path)
+		CSV.foreach(path, headers: true) do |row|
+			User.create(
+				name: row["name"],
+				age: row["age"],
+				address: ["address"]
+			)
+		end
+	end
+end
